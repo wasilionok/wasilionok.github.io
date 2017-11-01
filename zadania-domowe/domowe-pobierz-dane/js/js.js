b52ab2e829480da349a661c3cfd50ce92d0ded30
Agnieszka Wasilionok <agnieszka@wasilionok.pl>
2017-11-02 00:34:57 +0100

    1234

--- zadania-domowe/domowe-pobierz-dane/js/js.js
+++ zadania-domowe/domowe-pobierz-dane/js/js.js
@@ -2,11 +2,12 @@
 
 
 function pobierzDane() {
-
+$(document).ready(function () {
     $('button').after("<div id='dane-programisty'></div>");
     $.getJSON('http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108', function (data) {
         document.getElementById('dane-programisty').innerHTML = data.firma + "<br/>" + data.imie + "<br/>" + data.nazwisko + "<br/>" + data.zawod;
         console.log(data);
     });
 
+});
 }
