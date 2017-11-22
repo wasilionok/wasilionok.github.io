44e0fc39f62ae495a35ec6bbf6cac2fdce9ba059
Agnieszka Wasilionok <agnieszka@wasilionok.pl>
2017-11-22 22:48:01 +0100

    zadanie_scss

--- zadania-domowe/Zadanie_domowe_sass/app/js.js/js.js
+++ zadania-domowe/Zadanie_domowe_sass/app/js.js/js.js
@@ -1,18 +1,16 @@
-"use strict";
-$(function () {
-    //    Scroll menu color change
-        if($(window).scrollTop() >= 50) {
-            $('#main-nav').addClass('scroll')
-        } 
-        
-        $(window).scroll(function(){
-        if($(window).scrollTop() >= 50) {
-            $('#main-nav').addClass('scroll')
-            $('#main-nav').css({'transition': 'all 0.3s'});
-        } else {
-            $('#main-nav').removeClass('scroll');
-        }
-        
-        
-    });
+$(function(){
+     /*scroll menu color*/  
+     if($(window).scrollTop() >= 50) {
+        $('#main-nav').addClass('scroll');
+     }
+    
+
+  $(window).scroll(function(){
+    if($(window).scrollTop() >= 50) {
+        $('#main-nav').addClass('scroll');
+        $('main-nav').css({'transition':'all 0.3s'});
+    } else {
+        $('#main-nav').removeClass('scroll');
+    }
 });
+    
